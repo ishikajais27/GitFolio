72e3566693cf47517339342cbdf0f721a00c1ec8
ishikajais27 <ishikajais09876@gmail.com>
2025-05-19 12:08:07 +0530

    Fix the about me section of template 1

--- backend/controllers/templateController.cjs
+++ backend/controllers/templateController.cjs
@@ -1,3 +1,52 @@
+// const {
+//   loadTemplates,
+//   applyTemplateReplacements,
+// } = require('../utils/templateUtils.cjs')
+
+// exports.getTemplates = (req, res) => {
+//   try {
+//     const templatesDir = path.join(__dirname, '../../templates')
+//     const files = fs.readdirSync(templatesDir)
+
+//     const templates = files
+//       .filter((file) => file.endsWith('.md'))
+//       .map((file) => ({
+//         id: file.replace('.md', ''),
+//         name: file
+//           .replace('.md', '')
+//           .replace(/([a-z])([A-Z])/g, '$1 $2')
+//           .replace(/-/g, ' '),
+//         previewUrl: `/templates/previews/${file.replace('.md', '.png')}`,
+//       }))
+
+//     res.json(templates)
+//   } catch (err) {
+//     res.status(500).json({ error: 'Unable to read templates' })
+//   }
+// }
+
+// exports.generateTemplate = (req, res) => {
+//   const { templateId } = req.params
+//   const profileData = req.body
+
+//   try {
+//     const templates = loadTemplates()
+//     if (!templates[templateId]) {
+//       return res.status(404).json({ error: 'Template not found' })
+//     }
+
+//     const markdown = applyTemplateReplacements(
+//       templates[templateId],
+//       profileData
+//     )
+//     res.send(markdown)
+//   } catch (error) {
+//     res.status(500).json({
+//       error: 'Failed to generate template',
+//       details: error.message,
+//     })
+//   }
+// }const {
 const {
   loadTemplates,
   applyTemplateReplacements,
