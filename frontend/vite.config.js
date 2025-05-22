d3d56863e84aa635e160e3b1687aeee288a71b10
ishikajais27 <ishikajais09876@gmail.com>
2025-05-22 20:07:58 +0530

    Fix bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,27 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   optimizeDeps: {
+//     include: ['@mui/icons-material'],
+//   },
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   optimizeDeps: {
     include: ['@mui/icons-material'],
   },
