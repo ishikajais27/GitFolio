451c11d927dce6b0e6068195596871ae89f52f58
ishikajais27 <ishikajais09876@gmail.com>
2025-05-22 18:55:01 +0530

    Fix the problems occurs during deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-//   build: {
-//     outDir: 'dist',
-//     emptyOutDir: true,
-//   },
-// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
+  optimizeDeps: {
+    include: ['@mui/icons-material'],
   },
 })
