6ce8c826ac2dfcb7cea1343a615fedb9fe512f28
ishikajais27 <ishikajais09876@gmail.com>
2025-05-22 15:29:55 +0530

    Fix some bugs

---
+++ frontend/vite.config.js
@@ -0,0 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, './src'),
+//     },
+//   },
+//   build: {
+//     outDir: 'dist',
+//     emptyOutDir: true,
+//   },
+// })
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+})
